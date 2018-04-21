9bc702e20f8095d0ecb2415f9cc11b200ba16a25
Jumper222 <38570080+Jumper222@users.noreply.github.com>
2018-04-21 18:51:09 +0200

    Update Cylinder.java

--- Formule/src/prove/Cylinder.java
+++ Formule/src/prove/Cylinder.java
@@ -15,25 +15,23 @@ public class Cylinder {
         System.out.println("Which is the height of your cylinder?");
         double heigh = height.nextDouble();
         
-        double baseofcyl = bas;
-        double pigreek = 3.14;
         double basare = (bas * bas)* 3.14;
-        String basearea = "Your base area " + basare + " cm²";
+        String basearea = "Your base area " + basare + " cmÂ²";
         System.out.println(basearea);
         // Cylinder Base area calculator
         
         double sidare = 2 * 3.14 * bas * heigh;
-        String sidearea = "Your side area is " + sidare + "  cm²";
+        String sidearea = "Your side area is " + sidare + "  cmÂ²";
         System.out.println(sidearea);
         // Cylinder Side Area calculator
         
         double totarea = 2 * 3.14 * bas * (heigh + bas) ;
-        String totalArea = "Your total area is " + totarea + " cm²";
+        String totalArea = "Your total area is " + totarea + " cmÂ²";
         System.out.println(totalArea);
         // Cylinder Total Area calculator
         
         double vlm = basare * heigh;
-        String volume = "Your volume is " + vlm + " cm³";
+        String volume = "Your volume is " + vlm + " cmÂ³";
         System.out.println(volume);
         // Cylinder volume calculator
         
