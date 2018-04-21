a8faa727e8cf5136e5d21ee1c7ad493021e99175
RedRival <38570389+RedRival@users.noreply.github.com>
2018-04-22 00:41:59 +0200

    Add files via upload

---
+++ Formule/src/prove/EquilateralCylinder.java
@@ -0,0 +1,41 @@
+package prove;
+
+import java.util.Scanner;
+
+public class EquilateralCylinder {
+
+	public static void main(String[] args) {
+		// Code for calculate the Equilater Cylinder dimensions
+		
+		Scanner a = new Scanner(System.in);
+		System.out.println("Which is the radius of your circle?");
+		double radius = a.nextDouble();
+		
+		double basearea = 3.14 * (radius*radius);
+		String baseara = "Your base area is " + basearea + " cm²";
+		System.out.println(baseara);
+		// Base area formula calculator
+		
+		
+		double sidearea = 4 * 3.14 * (radius*radius);
+		String sideara = "Your side area is " + sidearea + " cm²";
+		System.out.println(sideara);
+		// Side area formula claculator
+		
+		
+		double totalarea = 6 * 3.14 * (radius * radius);
+		String totArea = "Your total area is " + totalarea + " cm²";
+		System.out.println(totArea);
+		// Total area formula calculator
+		
+		
+		double volume = 2 * 3.14 * (radius * radius * radius);
+		String vlm = "Your volume is " + volume + " cm³";
+		System.out.println(vlm);
+		// Volume formula calculator
+		
+		a.close();		
+
+	}
+
+}
